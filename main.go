afba5dd9e090e737ee41409507f2c64995227ba9
Nelson Germán Ghezzi <nelsonghezzi@users.noreply.github.com>
2021-03-25 13:08:35 -0300

    Loop examples instead of exiting after one selection/run

--- main.go
+++ main.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 	"os"
 	"strconv"
+	"strings"
 
 	"github.com/go-color-term/go-color-term-examples/examples"
 	"github.com/go-color-term/go-color-term/coloring"
@@ -27,26 +28,46 @@ func main() {
 		&examples.UtilityExtrasExample{},
 	}
 
-	fmt.Println("Select example:")
+	for {
+		fmt.Println("Select example (press ENTER to exit):")
 
-	for i, example := range examples {
-		fmt.Printf("%d) %s\n", i+1, example.GetTitle())
+		for i, example := range examples {
+			fmt.Printf("%d) %s\n", i+1, example.GetTitle())
+		}
+
+		scanner := bufio.NewScanner(os.Stdin)
+		scanner.Scan()
+
+		text := strings.TrimSpace(scanner.Text())
+		if text == "" {
+			fmt.Println(coloring.Sentence().
+				BoldStart().
+				Text("Thanks for trying ").
+				ItalicStart().
+				Color("go", coloring.BRIGHTBLUE).
+				Text("-").
+				Color("color", coloring.BRIGHTRED).
+				Text("-").
+				Color("term", coloring.BRIGHTGREEN).
+				ItalicEnd().
+				Text(". Goodbye!"))
+
+			os.Exit(1)
+		}
+
+		selectedExample, err := strconv.Atoi(text)
+		selectedExample--
+
+		if err != nil || selectedExample < 0 || selectedExample > len(examples)-1 {
+			fmt.Println(coloring.Bold("Example not found!"))
+
+			continue
+		}
+
+		fmt.Println(examples[selectedExample].GetDescription())
+
+		examples[selectedExample].Run()
+
+		fmt.Println()
 	}
-
-	scanner := bufio.NewScanner(os.Stdin)
-	scanner.Scan()
-
-	selectedExample, err := strconv.Atoi(scanner.Text())
-	selectedExample--
-
-	if err != nil || selectedExample < 0 || selectedExample > len(examples)-1 {
-		fmt.Println(coloring.Bold("Example not found!"))
-		os.Exit(1)
-	}
-
-	fmt.Println(examples[selectedExample].GetDescription())
-
-	examples[selectedExample].Run()
-
-	fmt.Println()
 }
